c6f1767b1cac19b73f8d86915a75fd6eaf727043
samin710 <smsaminii112@gmail.com>
2025-06-25 11:50:58 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+Website name: TradePort
+
+Live site URL: https://b2b-wholesale-platform-57595.web.app/
+
+Features:
+🎯 Users can add products after login.
+
+🎯 Users get informed about discounts in the banner section.
+
+🎯 Users get browse through every category and see the details about a particular product.
+
+🎯 Users get informed about discounts in the banner section.
+
+🎯 Users can buy any product, but:
+      👍 They have to purchase it at the minimum selling quantity.
+      👍 They cannot exceed the available quantity while purchasing.
+
+🎯 Users get see the product in both card and table format.
+
+🎯 Available section only contains the products which's minimum selling quantity is more than 100.
+
+🎯 Any user can edit any product.
+
+🎯 Only user can delete only his or her posted products not others.
+
+🎯 Users must be logged in to:
+      👍 Add new product.
+      👍 View all products.
+      👍 View their own products.
+      👍 View their own cart.
+
+🎯 Users can see their own purchased product in cart page and from there they can delete any of their purchase product, this increases the main available quantity of that product which the users delete form their cart page by the deleted amount .
+
+🎯 A theme toggle button switches the UI between dark and light modes.
+
+
+🎯 Used npm packages are:
+      👍 @tailwindcss/vite
+      👍 animate.css
+      👍 axios
+      👍 firebase
+      👍 framer-motion
+      👍 lottie-react 
+      👍 motion
+      👍 react-hook-form
+      👍 react-icon
+      👍 react-router
+      👍 react-toastify
+      👍 react-tooltip
+      👍 react
+      👍 swiper
---
+++ index.js
@@ -0,0 +1,229 @@
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@tradeport.i5k2jkx.mongodb.net/?retryWrites=true&w=majority&appName=TradePort`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const admin = require("firebase-admin");
+
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
+  "utf8"
+);
+const serviceAccount = JSON.parse(decoded);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const verifyFireBaseToken = async (req, res, next) => {
+  const authHeader = req.headers?.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+    req.decoded = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+};
+
+const verifyTokenEmail = (req, res, next) => {
+  const email = req.headers.email;
+
+  if (email !== req.decoded.email) {
+    return res.status(403).send({ message: "Forbidden access" });
+  }
+  next();
+};
+
+async function run() {
+  try {
+    const productsCollection = client.db("productsDB").collection("products");
+    const ordersCollection = client.db("productsDB").collection("orders");
+
+    // Products related apis
+    app.get(
+      "/products",
+      verifyFireBaseToken,
+      verifyTokenEmail,
+      async (req, res) => {
+        const result = await productsCollection.find().toArray();
+        res.send(result);
+      }
+    );
+
+    app.get("/products/category/:category", async (req, res) => {
+      const category = req.params.category;
+
+      const query = {
+        category: category,
+      };
+      const result = await productsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+<<<<<<< HEAD
+    app.get(
+      "/products/:id",
+      verifyFireBaseToken,
+      verifyTokenEmail,
+      async (req, res) => {
+        const query = { _id: new ObjectId(req.params.id) };
+        const result = await productsCollection.findOne(query);
+        res.send(result);
+      }
+    );
+
+    app.post(
+      "/products",
+      verifyFireBaseToken,
+      verifyTokenEmail,
+      async (req, res) => {
+        const result = await productsCollection.insertOne(req.body);
+        res.send(result);
+      }
+    );
+
+    app.delete(
+      "/products/:id",
+      verifyFireBaseToken,
+      verifyTokenEmail,
+      async (req, res) => {
+        const query = { _id: new ObjectId(req.params.id) };
+
+        const result = await productsCollection.deleteOne(query);
+        res.send(result);
+      }
+    );
+
+    app.put(
+      "/products/:id",
+      verifyFireBaseToken,
+      verifyTokenEmail,
+      async (req, res) => {
+        const filter = { _id: new ObjectId(req.params.id) };
+
+        const options = { upsert: true };
+
+        const updatedProduct = req.body;
+
+        const updateDoc = {
+          $set: updatedProduct,
+        };
+
+        const result = await productsCollection.updateOne(
+          filter,
+          updateDoc,
+          options
+        );
+        res.send(result);
+      }
+    );
+
+    app.patch("/products/:id", async (req, res) => {
+      const filter = { _id: new ObjectId(req.params.id) };
+
+      const { quantity, restore } = req.body;
+
+      const updateDoc = {
+        $inc: {
+          main_quantity: restore ? Math.abs(quantity) : -Math.abs(quantity),
+        },
+      };
+      const result = await productsCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // orders related apis
+    app.get("/orders", async (req, res) => {
+      const result = await ordersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get(
+      "/orders/buyer",
+      verifyFireBaseToken,
+      verifyTokenEmail,
+      async (req, res) => {
+        const email = req.query.email;
+
+        const query = {
+          buyerEmail: email,
+        };
+        const result = await ordersCollection.find(query).toArray();
+        res.send(result);
+      }
+    );
+
+    app.delete(
+      "/orders/:id",
+      verifyFireBaseToken,
+      verifyTokenEmail,
+      async (req, res) => {
+        const query = { _id: new ObjectId(req.params.id) };
+
+        const result = await ordersCollection.deleteOne(query);
+        res.send(result);
+      }
+    );
+
+    app.post("/orders", async (req, res) => {
+      const result = await ordersCollection.insertOne(req.body);
+=======
+    app.get("/categories", async (req, res) => {
+      const category = req.query.category; 
+
+      const query = {
+        category: category,
+      };
+      const result = await productsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/products", async (req, res) => {
+      const result = await productsCollection.insertOne(req.body);
+>>>>>>> 72ded35 (category wise api done)
+      res.send(result);
+    });
+
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("TradePort Server in cooking!!!");
+});
+
+app.listen(port, () => {
+  console.log(`TradePort server is running on port http://localhost:${port}`);
+});
